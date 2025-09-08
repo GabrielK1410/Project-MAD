42a931181e76273982b1c128fac845b13582cf51
GabrielK1410 <kaunanggabriel0@gmail.com>
2025-09-08 10:53:44 +0800

    profile

--- App.tsx
+++ App.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import { Image, StyleSheet } from 'react-native';
-import {
-  SafeAreaProvider,
-  SafeAreaView,
-  Scrollview,
-} from 'react-native-safe-area-context';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 const styles = StyleSheet.create({
   container: {
